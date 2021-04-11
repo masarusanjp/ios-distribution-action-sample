468f458194a6b2be68664d20c03649faaefd8cef
masarusanjp <masarusanplusplus@gmail.com>
2021-04-11 22:06:00 +0900

    init

---
+++ DistributionActionSample/DistributionActionSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistributionActionSampleApp.swift
+//  DistributionActionSample
+//
+//  Created by Ichikawa Masaru on 2021/04/11.
+//
+
+import SwiftUI
+
+@main
+struct DistributionActionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
